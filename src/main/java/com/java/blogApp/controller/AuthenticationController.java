4000727c69c584f1f4ab99cd38a3d9f6925cb599
Ahmed_Alhalwagy <ahmedhalwgy@gmail.com>
2024-06-21 16:10:04 +0300

    user and auth controller added

--- src/main/java/com/java/blogApp/controller/AuthenticationController.java
+++ src/main/java/com/java/blogApp/controller/AuthenticationController.java
@@ -1,4 +1,34 @@
 package com.java.blogApp.controller;
 
+import com.java.blogApp.dto.AuthentecationResponseModel;
+import com.java.blogApp.dto.AuthenticationRequestModel;
+import com.java.blogApp.dto.RegisterRequestModel;
+import com.java.blogApp.service.AuthService;
+import com.java.blogApp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthentecationResponseModel> register(@RequestBody RegisterRequestModel authRequest) {
+        return ResponseEntity.ok( authService.register(authRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthentecationResponseModel> login(@RequestBody AuthenticationRequestModel authentecationRequestModel){
+
+
+        return ResponseEntity.ok(authService.login(authentecationRequestModel));
+
+    }
+
+
 }
--- src/main/java/com/java/blogApp/controller/UserController.java
+++ src/main/java/com/java/blogApp/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.java.blogApp.controller;
 
+import com.java.blogApp.dto.UserResponseModel;
+import com.java.blogApp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+
+    private final UserService userService;
+
+
+    @GetMapping("")
+    public List<UserResponseModel> findAllUsers(){
+        return userService.findAllUsers();
+    }
+
+
 }
