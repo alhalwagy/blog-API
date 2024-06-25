b47fc47146a6f31b68d4f780c4bb950dbf7d5219
Ahmed_Alhalwagy <ahmedhalwgy@gmail.com>
2024-06-25 12:50:27 +0300

    user repository updated with findByEmail() and existByEmail()

--- src/main/java/com/java/blogApp/repository/UserRepository.java
+++ src/main/java/com/java/blogApp/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.java.blogApp.repository;
 import com.java.blogApp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+
+    Optional<User> findByEmail(String username);
+
+    boolean existsByEmail(String email);
 }
