a1843b9c362840f0cb31b6794449d6d909af8e0d
Ahmed_Alhalwagy <ahmedhalwgy@gmail.com>
2024-06-30 03:41:57 +0300

    fixed pug in postRepository in deletePost

--- src/main/java/com/java/blogApp/repository/PostPagingAndSortingRepository.java
+++ src/main/java/com/java/blogApp/repository/PostPagingAndSortingRepository.java
@@ -1,7 +1,11 @@
 package com.java.blogApp.repository;
 
 import com.java.blogApp.entity.Post;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -10,6 +14,9 @@ public interface PostPagingAndSortingRepository extends PagingAndSortingReposito
 
     Post save(Post post);
 
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM posts WHERE id = ?1", nativeQuery = true)
     void deleteById(int postId);
 
     List<Post> findAll();
